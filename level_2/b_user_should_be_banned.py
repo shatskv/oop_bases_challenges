61b389129fd8a56f0ac55dbafa9679495ed41465
Vladimir Shatskiy <vovaysach@gmail.com>
2023-12-09 04:04:54 +0300

    level_2 done

--- level_2/b_user_should_be_banned.py
+++ level_2/b_user_should_be_banned.py
@@ -7,8 +7,17 @@
        Пользователя стоит забанить, если его фамилия находится в SURNAMES_TO_BAN.
 """
 
+
 SURNAMES_TO_BAN = ['Vaughn', 'Wilhelm', 'Santaros', 'Porter', 'Smith']
 
 
 class User:
-    pass  # код писать тут
+    def __init__(self, name: str, surname: str, age: int) -> None:
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def should_be_banned(self, surnames_to_ban: list[str]) -> bool:
+        if self.surname in surnames_to_ban:
+            return True
+        return False
